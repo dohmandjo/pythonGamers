15246a656f556b8b5a828148c099762d85a4bdbd
Trevor Wayman <way16006@byui.edu>
2023-05-31 15:44:39 -0600

    Attempt to update sync

---
+++ game/settings.py
@@ -0,0 +1,8 @@
+
+"""
+    This file contains per-device settings. The default values are stored in `constants.py`.
+"""
+monitor = 0 # Which display to place window on.
+windowed_width = 1280
+windowed_height = 720
+fullscreen = False
