cc1446f4c2cb5e0700a1b986877696ee250a0fce
AlysaJarrell <cra20007@byui.edu>
2023-06-29 14:04:56 -0600

    comments added for platform generation

--- main.py
+++ main.py
@@ -87,35 +87,33 @@ def populate(entities=arcade.Scene()):
     # entities.add_sprite("stage", arcade.Sprite("res/stage/platform1.png", center_x=500, center_y=100))
     entities.add_sprite_list("stage", use_spatial_hash=True)
     # entities.add_sprite("stage", Stage("res/stage/platform1.png", 500, 100))
-    # TEST PLATFORM
     
+    
+    # creates floor for total length with randomized images
     for x in range(0, 12500, 100):
             floor = arcade.Sprite(constants.FLOOR_IMAGES[random.randint(0,3)])
             floor.center_x = x
             floor.center_y = constants.FLOOR_HEIGHT
             entities.add_sprite("stage", floor)
-
+    # adds a (4 pngs) platform at the first height so other heights can be reached 
     for x in range(800, 1056, 100):
         platform = arcade.Sprite(constants.FLOOR_IMAGES[random.randint(0,3)])
         platform.center_x = x
         platform.center_y = constants.PLATFORM_HEIGHTS[0]
         entities.add_sprite("stage", platform)
 
+    # continues randomized platforms at random (given) platform heights for rest of the map
     for x in range(1184, 12500, 100):
         randNum = random.randint(0,4)
+        # if 4 is the random int, it will leave a blank space instead of a platform
         if randNum == 4:
              continue
 
         platform = arcade.Sprite(constants.FLOOR_IMAGES[randNum])
         platform.center_x = x
-        platform.center_y = constants.PLATFORM_HEIGHTS[random.randint(0,2)] #
+        platform.center_y = constants.PLATFORM_HEIGHTS[random.randint(0,2)] #gives height of platform from list in Constants
         entities.add_sprite("stage", platform)
 
-    # for x in range(1568, 2080, 100):
-    #     platform = arcade.Sprite(constants.FLOOR_IMAGES[random.randint(0,3)])
-    #     platform.center_x = x
-    #     platform.center_y = constants.PLATFORM_HEIGHTS[random.randint(0,2)]
-    #     entities.add_sprite("stage", platform)
         
     return entities
 
